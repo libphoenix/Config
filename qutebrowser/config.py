8a3f3fbd96e30a3a437433806f15ad78cfba5dce
deepak patel <deepakpatelg16@gmail.com>
2022-07-07 08:11:42 +0530

    routine update

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,6 +1,7 @@
 import subprocess
 import os
 from qutebrowser.api import interceptor
+import dracula.draw
 
 """
 qutebrowser settings for video
@@ -9,6 +10,13 @@ https://qutebrowser.org/doc/help/settings.html
 """
 
 config.load_autoconfig(False)
+
+dracula.draw.blood(c, {
+    'spacing': {
+        'vertical': 6,
+        'horizontal': 8
+    }
+})
 # ================== Youtube Add Blocking ======================= {{{
 def filter_yt(info: interceptor.Request):
     """Block the given request if necessary."""
@@ -22,25 +30,3 @@ def filter_yt(info: interceptor.Request):
 
 
 interceptor.register(filter_yt)
-# }}}
-#c.content.blocking.method = 'adblock'
-#c.content.blocking.adblock.lists = [
-#        "https://easylist.to/easylist/easylist.txt",
-#        "https://easylist.to/easylist/easyprivacy.txt",
-#        "https://easylist.to/easylist/fanboy-social.txt",
-#        "https://secure.fanboy.co.nz/fanboy-annoyance.txt",
-#        "https://easylist-downloads.adblockplus.org/abp-filters-anti-cv.txt",
-#        "https://gitlab.com/curben/urlhaus-filter/-/raw/master/urlhaus-filter.txt",
-#        "https://pgl.yoyo.org/adservers/serverlist.php?showintro=0;hostformat=hosts",
-#        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/legacy.txt",
-#        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters.txt",
-#        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2020.txt",
-#        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2021.txt",
-#        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badware.txt",
-#        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/privacy.txt",
-#        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badlists.txt",
-#        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances.txt",
-#        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/resource-abuse.txt",
-#        "https://www.i-dont-care-about-cookies.eu/abp/",
-#        "https://secure.fanboy.co.nz/fanboy-cookiemonster.txt",
-#        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/unbreak.txt"]
