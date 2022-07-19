fcdb936c111356b949cda96b4d95e0f91a821691
deepak patel <deepakpatelg16@gmail.com>
2022-07-19 08:51:33 +0530

    routine update

--- emacs/init.el
+++ emacs/init.el
@@ -122,25 +122,12 @@
 (general-define-key
  "C-M-j" 'counsel-switch-buffer) 
 
-(defun dp/evil-hook ()
-  (dolist (mode '(custom-mode
-		  eshell-mode
-		  git-rebase-mode
-		  erc-mode
-		  circe-server-mode
-		  circe-chat-mode
-		  crice-query-mode
-		  sauron-mode
-		  term-mode))
-    (add-to-list 'evil-emacs-state-modes mode)))
-
 (use-package evil
   :init
   (setq evil-want-integration t)
   (setq evil-want-keybinding nil)
   (setq evil-want-C-u-scroll t)
   (setq evil-want-C-i-jump nil)
-  :hook (evil-mode . dp/evil-hook)
   :config
   (evil-mode 1)
   (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
