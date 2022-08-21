19f9706809c9a495e597539b99596937e2b7fc3d
deepak patel <deepakpatelg16@gmail.com>
2022-08-21 19:06:57 +0530

    removing unwanted file from qutebrowser config

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -2,30 +2,7 @@ import subprocess
 import os
 from qutebrowser.api import interceptor
 
-"""
-qutebrowser settings for video
-for more settings check out
-https://qutebrowser.org/doc/help/settings.html
-"""
 
 config.load_autoconfig(False)
 
 config.source('onedark.py')
-# ================== Youtube Add Blocking ======================= {{{
-c.content.blocking.adblock.lists = [ \
-        "https://easylist.to/easylist/easylist.txt", \
-        "https://easylist.to/easylist/easyprivacy.txt", \
-        "https://secure.fanboy.co.nz/fanboy-cookiemonster.txt", \
-        "https://easylist.to/easylist/fanboy-annoyance.txt", \
-        "https://secure.fanboy.co.nz/fanboy-annoyance.txt", \
-        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances.txt", \
-        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2020.txt", \
-        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/unbreak.txt", \
-        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/resource-abuse.txt", \
-        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/privacy.txt", \
-        "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters.txt" \
-        ]
-
-c.content.blocking.enabled = True
-c.content.blocking.hosts.lists = ['https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts']
-c.content.blocking.method = 'both'
