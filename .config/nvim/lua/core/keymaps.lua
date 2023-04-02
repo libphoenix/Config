19aa659251e3e140b442357647b4c284146c8a4c
Deepak Patel <me@deepakpatel.dev>
2023-04-02 16:22:22 +0530

    refact(nvim): remove commented code

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -58,7 +58,6 @@ keymap.set("n", "<leader>hw", ":HopWord<CR>") -- Hop to a word
 keymap.set("n", "<leader>hl", ":HopLine<CR>") -- Hop to a line
 
 -- terminal commands
--- keymap.set("t", "<leader>tt", "<C-\\><C-n><CMD>:FTermToggle<CR>") -- Toggle Fterm
 keymap.set("t", "<leader>tt", "<C-\\><C-n><CMD>:FTermToggle<CR>") -- Toggle Fterm
 keymap.set("n", "<leader>tt", ":FTermToggle<CR>") -- Toggle Fterm
 keymap.set("n", "<leader>tx", ":FTermExit<CR>") -- Close Fterm
